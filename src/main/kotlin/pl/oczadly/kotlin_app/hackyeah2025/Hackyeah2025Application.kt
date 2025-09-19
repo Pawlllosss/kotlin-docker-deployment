184884505a824124657e8b3e6fec096c578e20a8
Pawlllosss <oczadlypawe@gmail.com>
2025-09-19 14:56:13 +0200

    Set OpenAPI default

--- src/main/kotlin/pl/oczadly/kotlin_app/hackyeah2025/Hackyeah2025Application.kt
+++ src/main/kotlin/pl/oczadly/kotlin_app/hackyeah2025/Hackyeah2025Application.kt
@@ -1,8 +1,11 @@
 package pl.oczadly.kotlin_app.hackyeah2025
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@OpenAPIDefinition(servers = [Server(url = "/", description = "Default Server URL")])
 @SpringBootApplication
 class Hackyeah2025Application
 
