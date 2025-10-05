59aa89206e6ec53f68b836acc37774d01c937eac
Pawlllosss <oczadlypawe@gmail.com>
2025-10-05 09:17:54 +0200

    added sample data for previous day

--- src/main/java/pl/oczadly/kotlin_app/hackyeah2025/pension/entity/PensionCalculationAuditingRepository.java
+++ src/main/java/pl/oczadly/kotlin_app/hackyeah2025/pension/entity/PensionCalculationAuditingRepository.java
@@ -114,6 +114,78 @@ public class PensionCalculationAuditingRepository {
                         .accountProgression(List.of())
                         .build()
         ));
+
+        db.put(UUID.randomUUID(), new PensionCalculationAuditing(
+                LocalDateTime.now().minusDays(1),
+                new PensionRequest(55, "M", 15000.0, 2002, 2067, false, null, 200000.0, 10000.0, "02-173"),
+                PensionResponse.builder()
+                        .nominalPension(PensionDetails.builder()
+                                .withSickLeave(12000.0)
+                                .withoutSickLeave(12000.0)
+                                .replacementRate(58.0)
+                                .finalSalary(20690.0)
+                                .finalAveragePension(7800.0)
+                                .vsAveragePension(153.8)
+                                .delayedScenarios(List.of(
+                                        new DelayedScenario(1, 12500.0, 4.2),
+                                        new DelayedScenario(2, 13000.0, 8.3),
+                                        new DelayedScenario(5, 14500.0, 20.8)
+                                ))
+                                .extraYearsNeededForExpected(0)
+                                .build())
+                        .realPension(PensionDetails.builder()
+                                .withSickLeave(6500.0)
+                                .withoutSickLeave(6500.0)
+                                .replacementRate(54.0)
+                                .finalSalary(12040.0)
+                                .finalAveragePension(4540.0)
+                                .vsAveragePension(143.2)
+                                .delayedScenarios(List.of(
+                                        new DelayedScenario(1, 6750.0, 3.8),
+                                        new DelayedScenario(2, 7000.0, 7.7),
+                                        new DelayedScenario(5, 7800.0, 20.0)
+                                ))
+                                .extraYearsNeededForExpected(0)
+                                .build())
+                        .accountProgression(List.of())
+                        .build()
+        ));
+
+        db.put(UUID.randomUUID(), new PensionCalculationAuditing(
+                LocalDateTime.now().minusDays(1),
+                new PensionRequest(40, "F", 8000.0, 2010, 2040, true, 32, 50000.0, 5000.0, "95-100"),
+                PensionResponse.builder()
+                        .nominalPension(PensionDetails.builder()
+                                .withSickLeave(6200.0)
+                                .withoutSickLeave(6500.0)
+                                .replacementRate(52.0)
+                                .finalSalary(11900.0)
+                                .finalAveragePension(6200.0)
+                                .vsAveragePension(100.0)
+                                .delayedScenarios(List.of(
+                                        new DelayedScenario(1, 6500.0, 4.8),
+                                        new DelayedScenario(2, 6800.0, 9.7),
+                                        new DelayedScenario(5, 7500.0, 21.0)
+                                ))
+                                .extraYearsNeededForExpected(0)
+                                .build())
+                        .realPension(PensionDetails.builder()
+                                .withSickLeave(3900.0)
+                                .withoutSickLeave(4100.0)
+                                .replacementRate(49.0)
+                                .finalSalary(7960.0)
+                                .finalAveragePension(4130.0)
+                                .vsAveragePension(94.4)
+                                .delayedScenarios(List.of(
+                                        new DelayedScenario(1, 4050.0, 3.8),
+                                        new DelayedScenario(2, 4200.0, 7.7),
+                                        new DelayedScenario(5, 4650.0, 19.2)
+                                ))
+                                .extraYearsNeededForExpected(2)
+                                .build())
+                        .accountProgression(List.of())
+                        .build()
+        ));
     }
 
     public PensionCalculationAuditing save(PensionCalculationAuditing forecastResult) {
